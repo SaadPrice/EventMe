ce1e5fb8af42ada3301826b20024ff7257a325dd
Saad <educationalmindz@gmail.com>
2024-06-06 21:23:05 -0400

    created index.css  file.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Pages/home';
+import Events from './Pages/events';
+import Login from './components/Auth/login';
+import Register from './components/Auth/register';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, EventMe!</h1>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/events" component={Events} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
---
+++ Frontend/src/Pages/events.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventForm from '../components/Events/EventForm';
+import EventList from '../components/Events/EventList';
+
+const Events = () => (
+  <div>
+    <h1>Events</h1>
+    <EventForm />
+    <EventList />
+  </div>
+);
+
+export default Events;
