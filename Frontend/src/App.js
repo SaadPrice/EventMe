d35da48942a38fcd8e9e0caf2c52caf25cf6850f
Saad <educationalmindz@gmail.com>
2024-06-07 15:44:08 -0400

    added vercel.json file in frontend.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Events from './pages/Events.';
+import './App.scss';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/events" component={Events} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
 
