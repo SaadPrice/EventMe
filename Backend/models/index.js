f0c79c2596f1249f59df64119dc4e59b71c7be21
Saad <educationalmindz@gmail.com>
2024-06-07 18:04:21 -0400

    updated index.js.

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -2,6 +2,7 @@ const sequelize = require('../config/database');
 const User = require('./user');
 const Event = require('./event');
 const Ticket = require('./ticket');
+const SavedEvent = require('./savedEvent');
 
 User.hasMany(Event);
 Event.belongsTo(User);
@@ -12,9 +13,13 @@ Ticket.belongsTo(User);
 Event.hasMany(Ticket);
 Ticket.belongsTo(Event);
 
+User.belongsToMany(Event, { through: SavedEvent, as: 'SavedEvents' });
+Event.belongsToMany(User, { through: SavedEvent });
+
 sequelize.sync({ force: true }).then(() => {
   console.log('Database & tables created!');
 });
 
-module.exports = { User, Event, Ticket };
+module.exports = { User, Event, Ticket, SavedEvent };
+
 
