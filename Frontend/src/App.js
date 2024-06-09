29cb2a87f642cf419f766253758466b210cbe051
Saad <educationalmindz@gmail.com>
2024-06-09 10:49:45 -0400

    updated files.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,33 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Events from './pages/Events'; // Ensure the case matches the actual file name
 import LandingPage from './pages/LandingPage';
 import MyTickets from './pages/MyTickets';
 import MySavedEvents from './pages/MySavedEvents';
+import Navbar from './components/Navbar'; // Import the Navbar component
 import './App.scss'; // Ensure this path matches the location of App.scss
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/my-tickets" element={<MyTickets />} />
-        <Route path="/my-saved-events" element={<MySavedEvents />} />
-      </Routes>
+      <div id="root">
+        <Navbar />  {/* Add the Navbar component here */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/my-tickets" element={<MyTickets />} />
+          <Route path="/my-saved-events" element={<MySavedEvents />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
 
 export default App;
+
+
+
 
 
 
--- Frontend/src/pages/LandingPage.js
+++ Frontend/src/pages/LandingPage.js
@@ -1,16 +1,25 @@
+// src/pages/LandingPage.js
 import React from 'react';
-import './LandingPage.scss'; // Ensure this path is correct
+import './LandingPage.css'; // Import any specific styles for the LandingPage
 
-function LandingPage() {
+const LandingPage = () => {
   return (
     <div className="landing-page">
-      <img src="/images/eventme-logo.jpg" alt="EventMe Logo" className="landing-logo" />
-      <h1>Welcome to EventMe</h1>
+      <div className="logo-container">
+        <img src="/images/eventme-logo.jpg" alt="EventMe Logo" className="landing-logo" />
+        <h1>Welcome to EventMe</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default LandingPage;
+
+
+
+
+
+
 
 
 
