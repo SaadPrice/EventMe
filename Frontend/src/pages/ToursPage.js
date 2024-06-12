15b9bb843cae73da4cb69b84a1930e4711b796ec
Saad <educationalmindz@gmail.com>
2024-06-12 17:57:44 -0400

    created tours page.

---
+++ Frontend/src/pages/ToursPage.js
@@ -0,0 +1,22 @@
+// src/pages/ToursPage.js
+import React from 'react';
+import { tours } from '../mockData';
+import SavedEventButton from '../components/SavedEventButton';
+
+const ToursPage = () => {
+  return (
+    <div>
+      <h1>Tours</h1>
+      <ul>
+        {tours.map(tour => (
+          <li key={tour.id}>
+            {tour.name} - {tour.date} - {tour.location}
+            <SavedEventButton eventId={tour.id} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToursPage;
