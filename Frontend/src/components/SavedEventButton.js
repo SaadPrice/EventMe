80babb440b246ef43851f28ed4f1e4b5ecc82e63
Saad <educationalmindz@gmail.com>
2024-06-12 17:52:56 -0400

    created concerts file.

--- Frontend/src/components/SavedEventButton.js
+++ Frontend/src/components/SavedEventButton.js
@@ -1,22 +1,35 @@
+// src/components/SavedEventButton.js
+
 import React from 'react';
 import axios from 'axios';
 
-const SaveEventButton = ({ eventId }) => {
-  const handleSaveEvent = async () => {
+const SavedEventButton = ({ eventId }) => {
+  const handleSave = async () => {
     try {
       const token = localStorage.getItem('token');
+      if (!token) {
+        console.error('No token found');
+        return;
+      }
+
       const res = await axios.post('/api/saved-events/save', { eventId }, {
-        headers: { 'Authorization': token }
+        headers: { 'Authorization': `Bearer ${token}` }
       });
+
       console.log(res.data);
+      // Optionally, you can show a success message to the user
+      alert('Event saved successfully');
     } catch (err) {
       console.error(err);
+      // Optionally, you can show an error message to the user
+      alert('Failed to save event');
     }
   };
 
   return (
-    <button onClick={handleSaveEvent}>Save Event</button>
+    <button onClick={handleSave}>Save Event</button>
   );
 };
 
-export default SaveEventButton;
+export default SavedEventButton;
+
---
+++ Frontend/src/pages/ConcertsPage.js
@@ -0,0 +1,22 @@
+// src/pages/ConcertsPage.js
+import React from 'react';
+import { concerts } from '../mockData';
+import SavedEventButton from '../components/SavedEventButton';
+
+const ConcertsPage = () => {
+  return (
+    <div>
+      <h1>Concerts</h1>
+      <ul>
+        {concerts.map(concert => (
+          <li key={concert.id}>
+            {concert.name} - {concert.date} - {concert.location}
+            <SavedEventButton eventId={concert.id} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ConcertsPage;
